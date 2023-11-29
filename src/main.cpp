b8005b34e5d632277a4d008c72d0720d9c53c05a
ShereenKhoja <43831078+ShereenKhoja@users.noreply.github.com>
2023-11-29 14:24:36 -0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//******************************************************************************
+// File name:   main.cpp
+// Author:      CS Faculty
+// Date:        8/22/2023
+// Class:       CS150-xx (xx is your section number)
+// Assignment:  Hello World
+// Purpose:     Display the message Hello World to the screen
+// Hours:       0.25
+//******************************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  return EXIT_SUCCESS;
+}
