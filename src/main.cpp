7732cb38731508f99194247db71ab6a01bbcb9bc
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2023-12-05 16:47:45 +0000

    Added more TODOs

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,8 @@ struct Point {
 	double mX, mY;
 };
 
+// TODO#4: Write the function prototype for reading a single Point
+
 void readPoints (ifstream &inFile, Point sPoints[], int &size);
 void printPoint (const Point &sPoint);
 double getDistance (const Point &sPoint1, const Point &sPoint2);
@@ -42,19 +44,25 @@ int main () {
   Point sClosestPoint;
 	int size = 0;
 
+	// TODO#2: Create a Point variable sPoint and initialize the variable to 
+	//				 (5, 2) using an initializer list
+
+	// TODO#3: Call your function printPoint to print the Point from TODO#2
+
 	inFile.open ("data/points.txt");
 	if (inFile.fail ()) {
 		cout << "Error opening file" << endl;
 		exit (EXIT_FAILURE);
 	}
 
-	// TODO#1: Read in all points from the file
-	readPoints (inFile, sPoints, size);
+	// TODO#7: Read in all points from the file by uncommenting the next line
+	// readPoints (inFile, sPoints, size);
 
-	// TODO#2: Complete the function printPoint
+	// TODO#9: Find the point closest to the origin using getClosestToPoint
+	//				 The Point sClosestPoint is to contain this Point for the code
+	//				 below to work correctly.
 
-	// TODO#3: Find the point closest to the origin. I recommend writing a 
-  //         function
+
 	if (size > 0) {
 		cout << "Point closest to origin is: ";
 		printPoint (sClosestPoint);
@@ -76,8 +84,38 @@ Parameters:		sPoint - point to be displayed
 Returned:			None
 ***********************************************************************/
 void printPoint (const Point &sPoint) {
-	// TOOD#2: Write this function
+	// TOOD#1: Write this function
 }
+
+/***********************************************************************
+Function:			readPoint
+
+Description:	Reads a single Point from a file
+
+Parameters:		
+
+Returned:			None
+***********************************************************************/
+
+// TOOD#5: Write this function
+
+
+/***********************************************************************
+Function:			readPoints
+
+Description:	Reads each point into an array of structs. The file is
+							already open for reading.
+
+Parameters:		infile	- stream to read from
+							sPoints	- array of points
+							size		- number of points read into the array
+
+Returned:			None
+***********************************************************************/
+void readPoints (ifstream &inFile, Point sPoints[], int &size) {
+// TOOD#6: Write this function
+}
+
 
 /***********************************************************************
 Function:			getDistance
@@ -96,18 +134,5 @@ double getDistance (const Point &sPoint1, const Point &sPoint2) {
                pow ((sPoint2.mY - sPoint1.mY), SQUARE));
 }
 
-/***********************************************************************
-Function:			readPoints
-
-Description:	Reads each point into an array of structs. The file is
-							already open for reading.
-
-Parameters:		infile	- stream to read from
-							sPoints	- array of points
-							size		- number of points read into the array
-
-Returned:			None
-***********************************************************************/
-void readPoints (ifstream &inFile, Point sPoints[], int &size) {
-// TOOD#1: Write this function
-}
+// TODO#8: Write the function getClosestToPoint including documentation
+//				 Use the function getDistance in your solution
